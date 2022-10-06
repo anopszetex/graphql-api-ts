5e09144aae1373e9f3ca7c49262a29a608c06228
Andre Luis <andre.1357.luis@gmail.com>
2022-10-06 17:49:29 -0300

    🗃️ getting connections

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,26 @@ import './env';
 
 import { createServer, IncomingMessage, ServerResponse } from 'http';
 import { PostgreStrategy } from './strategies/postgreStrategy';
+import { createContainer } from './infra/connect';
 import logger from './infra/logger';
 
+const conn = createContainer(logger);
+
+conn.get('dev');
+conn.get('dev');
+conn.get('dev');
+conn.get('dev');
+conn.get('dev');
+conn.get('dev');
+
+const knex = PostgreStrategy(conn.get('dev'));
+
 const PORT = process.env.SERVER_PORT ?? 3940;
-
-const database = PostgreStrategy('dev');
-
 function handler(request: IncomingMessage, response: ServerResponse): void {
   logger.info('request received');
 
-  database
-    .getUser({ table: 'users', input: { id: 1 } })
+  knex
+    .getUser({ table: 'users', input: { id: 1 }, columns: ['id'] })
     .then(result => {
       logger.info({ result }, 'users');
       return null;
