c5ebf6e13c46699fb4cf4df45a33d854f44e9642
Andre Luis <andre.1357.luis@gmail.com>
2022-10-06 17:48:10 -0300

    ✨ improve connect to database

--- src/infra/connect.ts
+++ src/infra/connect.ts
@@ -1,27 +1,36 @@
 import { Knex, knex } from 'knex';
 import { getConfig } from './config';
+import { Logger } from 'pino';
 
-const connect = (database: string): Knex => {
-  const extra = {
+function connect(database: string, logger: Logger): Knex {
+  const extra: Partial<Knex.Config> = {
     pool: {
       afterCreate: (_: Knex, done: () => void) => {
-        console.log('afterCreate');
+        logger.info(`Connected to ${database}`);
         done();
       },
     },
   };
 
   return knex(getConfig(database, extra));
-};
+}
 
 interface DataBaseContainer {
   get: (datasource: string) => Knex;
 }
 
-export function createContainer(): DataBaseContainer {
+export function createContainer(logger: Logger): DataBaseContainer {
+  const cache = new Map<string, Knex>();
+
   return {
     get(datasource): Knex {
-      return connect(datasource);
+      if (!cache.has(datasource)) {
+        cache.set(datasource, connect(datasource, logger));
+      }
+
+      logger.debug(`Connection ${datasource} retrieved from cache`);
+
+      return cache.get(datasource) as Knex;
     },
   };
 }
