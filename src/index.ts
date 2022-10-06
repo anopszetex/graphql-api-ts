f3d4982a8b6d548471915db28249772df2ba8be3
Andre Luis <andre.1357.luis@gmail.com>
2022-10-06 15:29:03 -0300

    ⚗️ perform experiments

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// import './env';
+import './env';
 
 import { createServer, IncomingMessage, ServerResponse } from 'http';
 import { PostgreStrategy } from './strategies/postgreStrategy';
@@ -6,13 +6,13 @@ import logger from './infra/logger';
 
 const PORT = process.env.SERVER_PORT ?? 3940;
 
-function handler(request: IncomingMessage, response: ServerResponse): void {
-  const database = PostgreStrategy('dev');
+const database = PostgreStrategy('dev');
 
+function handler(request: IncomingMessage, response: ServerResponse): void {
   logger.info('request received');
 
   database
-    .findOne('users', {})
+    .getUser({ table: 'users', input: { id: 1 } })
     .then(result => {
       logger.info({ result }, 'users');
       return null;
@@ -26,6 +26,5 @@ function handler(request: IncomingMessage, response: ServerResponse): void {
 }
 
 createServer(handler).listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server running at http://localhost/${PORT}`);
+  logger.info(`🚀 Server running at http://localhost/${PORT}`);
 });
