39844daf0149cd645c91bfff59463498f7713270
Andre Luis <andre.1357.luis@gmail.com>
2022-10-06 23:55:58 -0300

    ✨ add mongoose config

--- src/infra/config.ts
+++ src/infra/config.ts
@@ -1,5 +1,5 @@
 import { Knex } from 'knex';
-import { defaultsDeep } from 'lodash';
+import { defaultsDeep, isEmpty } from 'lodash';
 
 const config = Object.freeze({
   client: 'pg',
@@ -13,10 +13,22 @@ const config = Object.freeze({
   pool: { min: 0, max: 5, idleTimeoutMillis: 60000 },
 });
 
-export function getConfig(database: string, extra = {}): Knex.Config {
+const options = Object.freeze({
+  user: process.env.DB_USER_MONGO ?? 'root',
+  password: process.env.DB_PASS_MONGO ?? 'root',
+  host: process.env.DB_HOST_MONGO ?? '127.0.0.1',
+  database: process.env.DB_NAME_MONGO ?? 'dev',
+  port: process.env.DB_PORT_MONGO ?? '27017',
+});
+
+export function getKnexConfig(database: string, extra = {}): Knex.Config {
   return defaultsDeep(
     { connection: { database } },
     extra,
     config
   ) as Knex.Config;
+}
+
+export function getMongooseConfig(): string {
+  return `mongodb://${options.user}:${options.password}@${options.host}:${options.port}`;
 }
