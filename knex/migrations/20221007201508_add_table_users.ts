5a43863cdbd3612952510248fe69bd532ab6ab4e
Andre Luis <andre.1357.luis@gmail.com>
2022-10-08 01:26:40 -0300

    🗃️ fix column email
    set email column

--- knex/migrations/20221007201508_add_table_users.ts
+++ knex/migrations/20221007201508_add_table_users.ts
@@ -4,6 +4,7 @@ export async function up(knex: Knex): Promise<void> {
   return await knex.schema.createTable('users', function (table) {
     table.increments('id').primary().unsigned();
     table.string('username', 100).nullable();
+    table.string('email', 225).nullable();
     table.string('first_name', 100).nullable();
     table.string('last_name', 100).nullable();
     table.string('password', 225).nullable();
