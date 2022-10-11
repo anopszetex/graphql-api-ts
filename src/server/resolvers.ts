075d2c10c378278b106cc449ede34fb9e7e5e470
Andre Luis <andre.1357.luis@gmail.com>
2022-10-10 23:42:00 -0300

    ✨ add resolvers

---
+++ src/server/resolvers.ts
@@ -0,0 +1,13 @@
+import { loadFiles } from '@graphql-tools/load-files';
+import { mergeResolvers } from '@graphql-tools/merge';
+import { IResolvers } from '@graphql-tools/utils/typings';
+
+import path from 'node:path';
+
+export async function buildResolvers(): Promise<IResolvers> {
+  const resolversArray = await loadFiles(
+    path.join(__dirname, '../resolvers/**/index.js')
+  );
+
+  return mergeResolvers(resolversArray);
+}
