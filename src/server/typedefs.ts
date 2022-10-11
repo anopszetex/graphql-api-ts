aaececadc60e57beffbee3caa76f528ad5e99a74
Andre Luis <andre.1357.luis@gmail.com>
2022-10-10 23:42:26 -0300

    ✨ add typedefs

---
+++ src/server/typedefs.ts
@@ -0,0 +1,13 @@
+import { DocumentNode } from 'graphql';
+import { loadFiles } from '@graphql-tools/load-files';
+import { mergeTypeDefs } from '@graphql-tools/merge';
+
+import path from 'node:path';
+
+export async function buildTypeDefs(): Promise<DocumentNode> {
+  const typesArray = await loadFiles(path.join(__dirname, '../schema'), {
+    extensions: ['graphql'],
+  });
+
+  return mergeTypeDefs(typesArray);
+}
