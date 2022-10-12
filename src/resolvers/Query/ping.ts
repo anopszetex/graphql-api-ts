6fbe2f95bccfc87a159382fecf7d0c8fb8b1b9eb
Andre Luis <andre.1357.luis@gmail.com>
2022-10-12 00:50:29 -0300

    ♻️ query.ping

--- src/resolvers/Query/ping.ts
+++ src/resolvers/Query/ping.ts
@@ -1,17 +1,3 @@
-import { Knex } from 'knex';
-import { filter } from 'lodash';
-import { UserPostgre } from './../../strategies/types';
-
-interface ITemp {
-  knex: Knex;
-}
-
-export async function ping(root: any, args: any, ctx: ITemp): Promise<string> {
-  const result = (await ctx.knex('users')) as UserPostgre[];
-
-  const onlyActive = filter(result, item => item.is_active);
-
-  console.log(onlyActive.length);
-
+export function ping(): string {
   return 'pong';
 }
